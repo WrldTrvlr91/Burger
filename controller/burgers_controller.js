cb62ecbbdf8a5064754c4aac2a7b41c19c013b83
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-22 21:19:51 +0800

    Controller file updated to include routes

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -2,3 +2,42 @@ const express = require("express");
 
 const router = express.Router();
 
+//Import the model to use its functions
+const burger = require("../models/burger.js");
+
+//All routes 
+
+router.get("/", function (req, res){
+    burger.all(function(output){
+        var burgerObject = {
+            burgers: output
+        };
+        res.render("index", burgerObject);
+    });
+});
+
+router.post("/api/burger", function(req, res){
+    burger.insert(["burger_name", "devoured"], [req.body.name, "FALSE"], function(output){
+        res.json({ id: output.insertId });
+    });
+});
+
+router.put("/api/burger/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(output) {
+            if (output.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controller/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    
+    console.log("Server listening on: " + PORT);
+});
+

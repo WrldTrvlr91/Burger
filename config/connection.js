137c698ff2952048e278d568e457612e888f37fa
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-27 20:24:08 +0800

    updated heroku detail in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 //Require mysql
 var mysql = require("mysql");
 
+var db;
+
 //Create conenction to the database
-var db = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+db = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "fj5867HJC74^$VBR#D5677b",
   database: "burgers_db"
 });
+};
 
 //Connect to the database
 db.connect(function(err) {
